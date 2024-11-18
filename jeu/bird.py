25f9eb6175de719076f2b3ed92f9997dd9941dc4
Macéo Tetelin <maceo.tetelin@student-cs.fr>
2024-11-18 14:55:40 +0100

    push oiseau

--- jeu/bird.py
+++ jeu/bird.py
@@ -1,49 +1,63 @@
 import sys
 import pygame
+import random as rd
+
+fps = 60
 
 
 class Bird():
-    def __init__(self, x, y, image1, image2):
+    def __init__(self, x, y, image1, image2, image3, image4, gravity):
         self.images = [pygame.image.load(
-            'image1'), pygame.image.load('image2')]
+            image1), pygame.image.load(image2), pygame.image.load(image3), pygame.image.load(image4)]
         self.index = 0
         self.image = self.images[self.index]
         self.rect = self.image.get_rect(center=(x, y))
         self.movement = 0
-        self.gravity = 0.5
+        self.gravity = 30*gravity/fps
 
+    # pour réinitialiser la position de l'oiseau si jamais on le réutilise 2 fois d'affilée
+    def reset(self, x, y):
+        self.rect.center = (x, y)
+        self.movement = 0
+        self.index = 0
+        self.image = self.images[self.index]
+
+    # gère les sauts
     def flap(self):
-        self.movement = -10
+        self.movement = -10*30/fps
 
+    # rotation de l'oiseau en fonction de sa vitesse de chute
+    def rotate(self):
+        # Limiter l'angle de rotation pour éviter des rotations excessives
+        max_rotation = 25
+        min_rotation = -90
+        rotation_angle = max(
+            min(self.movement * -2, max_rotation), min_rotation)
+        self.image = pygame.transform.rotozoom(
+            self.images[self.index], rotation_angle, 1)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    # gère l'animation de l'oiseau et la chute à chaque instant
     def update(self):
         self.movement += self.gravity
         self.rect.y += self.movement
-        self.index = (self.index + 1) % len(self.images)
+        tirage = rd.random()
+        if tirage > 0.5:
+            self.index = (self.index + 1) % len(self.images)
         self.image = self.images[self.index]
+        self.rotate()
 
+    # afficher l'oiseau sur l'écran
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
 
-pygame.init()
-screen = pygame.display.set_mode((400, 600))
-clock = pygame.time.Clock()
+'''
+Exemple de création d'un oiseau
 
-bird = Bird(50, 300, )
+Oiseau standard :
+bird = Bird(40, 300, './sprites/bird_base/bird_base1.png', './sprites/bird_base/bird_base2.png',
+            './sprites/bird_base/bird_base3.png', './sprites/bird_base/bird_base4.png', 0.5)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bird.flap()
 
-    bird.update()
-
-    screen.fill((0, 0, 0))  # Effacer l'écran
-    bird.draw(screen)  # Dessiner l'oiseau
-
-    pygame.display.update()
-    clock.tick(30)
+'''
