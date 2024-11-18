c3df03d9bffa6a360a34dbf0ab8d08614f119142
Banzet Thomas <thomas.banzet@student-cs.fr>
2024-11-18 10:52:41 +0100

    Création de la classe de jeu permettant la boucle de jeu

--- jeu/game_loop.py
+++ jeu/game_loop.py
@@ -1,8 +1,32 @@
 import pygame
-running=True
-while running:
-    screen=pygame.display.set_mode((800,600))
-    for event in pygame.event.get():
-        if event==quit:
-            running=False
+from bird import Bird
+
+class Game:
+    def __init__(self,screen):
+        self.screen=screen
+        self.running=True
+        self.bird=Bird()
+        self.clock=pygame.time.Clock()
+        self.bird=Bird()
     
+    def handling_events(self):
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                self.running=False
+    
+    def update(self):
+        pass
+    
+    def display(self):
+        pass
+             
+    def run(self):
+        while self.running:
+            self.handling_events()
+            self.update()
+            self.display()
+
+
+screen=pygame.display.set_mode((1080,720))
+game=Game(screen)
+game.run()
