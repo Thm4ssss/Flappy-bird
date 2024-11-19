3a713fa98768124c6c847b7eb2ff6352308fe306
Macéo Tetelin <maceo.tetelin@student-cs.fr>
2024-11-19 10:06:43 +0100

    changement fond

--- jeu/game_loop.py
+++ jeu/game_loop.py
@@ -3,8 +3,9 @@ from bird import Bird
 from pipe import Pipe
 import random
 
+
 class Game:
-    #Constructeur de la classe game, permet de définir toutes les variables utiles au bon fonctionnement du jeu
+    # Constructeur de la classe game, permet de définir toutes les variables utiles au bon fonctionnement du jeu
     def __init__(self, width, length):
         pygame.init()
         self.width = width
@@ -12,22 +13,24 @@ class Game:
         self.screen = pygame.display.set_mode((width, length))
         self.running = True
         self.bird = Bird(40, 300, './sprites/bird_base/bird_base1.png', './sprites/bird_base/bird_base2.png',
-                         './sprites/bird_base/bird_base3.png', './sprites/bird_base/bird_base4.png', 0.5,length)
+                         './sprites/bird_base/bird_base3.png', './sprites/bird_base/bird_base4.png', 0.5, length)
         self.clock = pygame.time.Clock()
-        self.background = pygame.image.load("sprites/fond.png").convert()
-        self.pipes = [Pipe(self.width, random.randint(60,  100))]  # Générer un tuyau avec une hauteur aléatoire
+        self.background = pygame.image.load(
+            "Background/fond_base.png").convert()
+        # Générer un tuyau avec une hauteur aléatoire
+        self.pipes = [Pipe(self.width, random.randint(60,  100))]
         self.pipe_spawn_timer = 0  # Timer pour générer les tuyaux
-    
-    #Permet la gestion des intéractions entre l'utilisateur et le jeu
+
+    # Permet la gestion des intéractions entre l'utilisateur et le jeu
     def handling_events(self):
-        for event in pygame.event.get(): 
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     self.bird.flap()
 
-    #Permet la mise à jour des différents éléments du jeu
+    # Permet la mise à jour des différents éléments du jeu
     def update(self):
         self.bird.update()  # Mettre à jour l'oiseau
         for pipe in self.pipes:
@@ -41,14 +44,14 @@ class Game:
         if self.pipe_spawn_timer > 1500:  # Nouveau tuyau toutes les 1.5 secondes
             self.pipes.append(Pipe(self.width, random.randint(100, 300)))
             self.pipe_spawn_timer = 0
- 
+
     def check_collisions(self):
         for pipe in self.pipes:
             if pipe.check_collision(self.bird.rect):  # Si collision avec un tuyau
                 self.running = False
-        #On vérifie si l'oiseau touche le sol, si c'est le cas il a perdu
-        if self.bird.rect.bottom>=self.length:
-            self.running=False
+        # On vérifie si l'oiseau touche le sol, si c'est le cas il a perdu
+        if self.bird.rect.bottom >= self.length:
+            self.running = False
 
     def display(self):
         self.screen.blit(self.background, (0, 0))  # Dessiner le fond
@@ -56,7 +59,7 @@ class Game:
         for pipe in self.pipes:
             pipe.display_pipe(self.screen)  # Dessiner les tuyaux
         pygame.display.flip()
-    
+
     def run(self):
         while self.running:
             self.clock.tick(60)  # Limiter à 60 FPS
@@ -65,10 +68,8 @@ class Game:
             self.check_collisions()  # Vérification des collisions
             self.display()  # Affichage à l'écran
         pygame.quit()
-        
-    
+
 
 # Lancer le jeu
 game = Game(950, 520)
 game.run()
-
