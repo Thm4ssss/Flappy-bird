c828a9f5bf4be134a70ac7b75d3e72dcd2269f32
Banzet Thomas <thomas.banzet@student-cs.fr>
2024-11-19 14:03:09 +0100

    Probleme tuyau réglé: jeu complet fini

--- jeu/game_loop.py
+++ jeu/game_loop.py
@@ -16,13 +16,13 @@ class Game:
                          './sprites/bird_base/bird_base3.png', './sprites/bird_base/bird_base4.png', 0.5, length)
         self.clock = pygame.time.Clock()
         self.background = pygame.image.load("Backgrounds/fond_base.png").convert()
-        self.pipes = [Pipe(self.width, random.randint(100,  300),self.length)]  # Générer un tuyau avec une hauteur aléatoire
+        self.pipes = [Pipe(self.width, random.randint(100,200),self.length,random.randint(100,300))]  # Générer un tuyau avec une hauteur aléatoire
         self.pipe_spawn_timer = 0  # Timer pour générer les tuyaux
 
     # Permet la gestion des intéractions entre l'utilisateur et le jeu
     def handling_events(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: 
                 self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
@@ -40,9 +40,9 @@ class Game:
         # Générer de nouveaux tuyaux toutes les X millisecondes
         self.pipe_spawn_timer += self.clock.get_time()
         if self.pipe_spawn_timer > 1500:  # Nouveau tuyau toutes les 1.5 secondes
-            self.pipes.append(Pipe(self.width, random.randint(100, 300),self.length))
+            self.pipes.append(Pipe(self.width, random.randint(100, 200),self.length,random.randint(100,300)))
             self.pipe_spawn_timer = 0
-
+ 
     def check_collisions(self):
         for pipe in self.pipes:
             if pipe.check_collision(self.bird.rect):  # Si collision avec un tuyau
--- jeu/pipe.py
+++ jeu/pipe.py
@@ -6,21 +6,22 @@ image_tuyau_bas = pygame.image.load("./sprites/tuyaux/image_tuyau_bas.png")
 
 
 class Pipe:
-    def __init__(self,screen_width,gap,screen_heigth,image_top=image_tuyau_haut,image_bottom=image_tuyau_bas):
+    def __init__(self,screen_width,gap,screen_height,top_pipe_height,image_top=image_tuyau_haut,image_bottom=image_tuyau_bas):
         #Définition des variables utiles d'un tuyau, tel que la position (coin haut gauche, la vitesse de déplacement ou encore l'écart entre deux tuyaux)
         self.x=screen_width
         self.y=0
         self.speed=3
         self.gap=gap
-        self.height=(screen_heigth-gap)/2
-        self.image_top=pygame.transform.scale(image_top,(90,self.height))
-        self.image_bottom=pygame.transform.scale(image_bottom,(90,self.height))
+        self.top_height=top_pipe_height
+        self.bottom_height=screen_height-self.top_height-self.gap
+        self.image_top=pygame.transform.scale(image_top,(90,self.top_height))
+        self.image_bottom=pygame.transform.scale(image_bottom,(90,self.bottom_height))
         self.width=self.image_top.get_width()
         
         
         #Définition de la hitbox
-        self.top_rect=pygame.Rect(self.x,self.y,self.width,self.height)
-        self.bottom_rect=pygame.Rect(self.x,self.height+self.gap,self.width,self.height)
+        self.top_rect=pygame.Rect(self.x,self.y,self.width,self.top_height)
+        self.bottom_rect=pygame.Rect(self.x,self.top_height+self.gap,self.width,self.bottom_height)
         
     def update_pipe(self):
         # Déplacement horizontal du tuyau
@@ -37,4 +38,4 @@ class Pipe:
     def display_pipe(self, screen):
         # On affiche les tuyaux
         screen.blit(self.image_top, (self.x, self.y))
-        screen.blit(self.image_bottom, (self.x, self.height+self.gap))
+        screen.blit(self.image_bottom, (self.x, self.top_height+self.gap))
