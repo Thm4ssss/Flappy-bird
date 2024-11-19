592108c744dc2bd43f9617b06e574536d326c474
Macéo Tetelin <maceo.tetelin@student-cs.fr>
2024-11-19 14:54:21 +0100

    fond jungle ajout

--- jeu/game_loop.py
+++ jeu/game_loop.py
@@ -1,11 +1,8 @@
 import pygame
 from bird import Bird
 from pipe import Pipe
-<<<<<<< HEAD
 from ground import ground
-=======
 from background import Background
->>>>>>> Macéo_bg
 import random
 
 
@@ -20,22 +17,17 @@ class Game:
         self.bird = Bird(40, 300, './sprites/bird_base/bird_base1.png', './sprites/bird_base/bird_base2.png',
                          './sprites/bird_base/bird_base3.png', './sprites/bird_base/bird_base4.png', 0.5, length)
         self.clock = pygame.time.Clock()
-<<<<<<< HEAD
-        self.background = pygame.image.load("Backgrounds/fond_base.png").convert()
-        self.pipes = [Pipe(self.width, random.randint(100,200),self.length,random.randint(100,300))]  # Générer un tuyau avec une hauteur aléatoire
-=======
         self.background = Background('jungle')
-        # Générer un tuyau avec une hauteur aléatoire
-        self.pipes = [Pipe(self.width, random.randint(60,  100))]
->>>>>>> Macéo_bg
+        self.pipes = [Pipe(self.width, random.randint(100, 200), self.length, random.randint(
+            100, 300))]  # Générer un tuyau avec une hauteur aléatoire
         self.pipe_spawn_timer = 0  # Timer pour générer les tuyaux
-        self.ground = ground(self.screen) # Importe le sol
-        self.dev = True
+        self.ground = ground(self.screen)  # Importe le sol
+        self.dev = False
 
     # Permet la gestion des intéractions entre l'utilisateur et le jeu
     def handling_events(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
+            if event.type == pygame.QUIT:
                 self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
@@ -44,7 +36,7 @@ class Game:
     # Permet la mise à jour des différents éléments du jeu
     def update(self):
         self.bird.update()  # Mettre à jour l'oiseau
-        self.ground.update() # Met à jour le sol
+        self.ground.update()  # Met à jour le sol
         for pipe in self.pipes:
             pipe.update_pipe()  # Mettre à jour les tuyaux
         self.background.update()  # Mettre à jour le background
@@ -54,9 +46,10 @@ class Game:
         # Générer de nouveaux tuyaux toutes les X millisecondes
         self.pipe_spawn_timer += self.clock.get_time()
         if self.pipe_spawn_timer > 1500:  # Nouveau tuyau toutes les 1.5 secondes
-            self.pipes.append(Pipe(self.width, random.randint(100, 200),self.length,random.randint(100,300)))
+            self.pipes.append(Pipe(self.width, random.randint(
+                100, 200), self.length, random.randint(100, 300)))
             self.pipe_spawn_timer = 0
- 
+
     def check_collisions(self):
         for pipe in self.pipes:
             if pipe.check_collision(self.bird.rect):  # Si collision avec un tuyau
@@ -66,29 +59,23 @@ class Game:
             self.running = False
 
     def display(self):
-<<<<<<< HEAD
         if self.dev:
-            self.screen.fill((0,0,0))
-            pygame.draw.rect(self.screen, (255,0,0), self.bird.rect, 2)
-            pygame.draw.rect(self.screen, (255,255,0), self.ground.rect, 2)
+            self.screen.fill((0, 0, 0))
+            pygame.draw.rect(self.screen, (255, 0, 0), self.bird.rect, 2)
+            pygame.draw.rect(self.screen, (255, 255, 0), self.ground.rect, 2)
             for pipe in self.pipes:
-                pygame.draw.rect(self.screen, (255,255,255), pipe.top_rect, 2)
-                pygame.draw.rect(self.screen, (255,255,255), pipe.bottom_rect, 2)
+                pygame.draw.rect(self.screen, (255, 255, 255),
+                                 pipe.top_rect, 2)
+                pygame.draw.rect(self.screen, (255, 255, 255),
+                                 pipe.bottom_rect, 2)
             pygame.display.flip()
         else:
-            self.screen.blit(self.background, (0, 0))  # Dessiner le fond
+            self.screen.blit(self.background.image, (0, 0))  # Dessiner le fond
             self.bird.draw(self.screen)  # Dessiner l'oiseau
             for pipe in self.pipes:
                 pipe.display_pipe(self.screen)  # Dessiner les tuyaux
-            self.ground.draw(self.screen) # Dessine le sol
+            self.ground.draw(self.screen)  # Dessine le sol
             pygame.display.flip()
-=======
-        self.screen.blit(self.background.image, (0, 0))  # Dessiner le fond
-        self.bird.draw(self.screen)  # Dessiner l'oiseau
-        for pipe in self.pipes:
-            pipe.display_pipe(self.screen)  # Dessiner les tuyaux
-        pygame.display.flip()
->>>>>>> Macéo_bg
 
     def run(self):
         while self.running:
