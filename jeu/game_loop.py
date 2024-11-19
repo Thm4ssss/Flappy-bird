25a81a61476bae74255631cf01aca70b1052057e
Arthur <arthur.viallix@student-cs.fr>
2024-11-19 15:27:44 +0100

    ajout du son

--- jeu/game_loop.py
+++ jeu/game_loop.py
@@ -9,6 +9,7 @@ class Game:
     # Constructeur de la classe game, permet de définir toutes les variables utiles au bon fonctionnement du jeu
     def __init__(self, width, length):
         pygame.init()
+        pygame.mixer.music.play (- 1)
         self.width = width
         self.length = length
         self.screen = pygame.display.set_mode((width, length))
@@ -20,16 +21,18 @@ class Game:
         self.pipes = [Pipe(self.width, random.randint(100,200),self.length,random.randint(100,300))]  # Générer un tuyau avec une hauteur aléatoire
         self.pipe_spawn_timer = 0  # Timer pour générer les tuyaux
         self.ground = ground(self.screen) # Importe le sol
-        self.dev = True
+        self.dev = False
 
     # Permet la gestion des intéractions entre l'utilisateur et le jeu
     def handling_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 self.running = False
+                pygame.mixer.music.stop()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     self.bird.flap()
+                    pygame.mixer.Sound.play( Flap_sound ) 
 
     # Permet la mise à jour des différents éléments du jeu
     def update(self):
@@ -81,6 +84,13 @@ class Game:
             self.display()  # Affichage à l'écran
         pygame.quit()
 
+
+#Ajout de la musque
+pygame.mixer.init()
+Flap_sound = pygame.mixer.Sound( "son/Flap.wav" )
+Flap_sound.set_volume(0.6)
+pygame.mixer.music.load( "son/musique.wav" ) 
+pygame.mixer.music.set_volume(0.4)
 
 # Lancer le jeu
 game = Game(900, 520)
