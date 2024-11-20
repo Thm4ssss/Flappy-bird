c93e80275020296ef4fb2a6255363b79e30f5d94
De Lencquesaing Gabriel <gabriel.delencquesaing@student-cs.fr>
2024-11-20 16:04:00 +0100

    powerup

--- jeu/power-up.py
+++ jeu/power-up.py
@@ -1,8 +1,9 @@
 import pygame
 import random
+from abc import ABC
+from game_loop import inverse_gravity
 
-
-class Powerup:
+"""class Powerup:
     def __init__(self, x, y, duration):
         self.image = pygame.image.load('powerup.png')
         self.rect = self.image.get_rect(center=(x, y))
@@ -27,4 +28,45 @@ class Powerup:
         self.apply(bird)
 
     def deactivate(self, bird):
-        self.active = False
+        self.active = False"""
+
+class PowerUp(ABC):
+    def __init__(self, screen_width, screen_height, height,image_path ):
+        self.x=screen_width
+        self.y=0
+        self.speed=3
+        self.height=height
+        self.image=pygame.transform.scale(image_path,(90,self.height))
+        self.width=self.image.get_width()
+
+        #définition de la hitbox
+        self.rect=pygame.Rect(self.x,self.y,self.width,self.height)
+    
+    def update_powerup(self):
+        # Déplacement horizontal du powerup
+        self.x -= self.speed
+
+        # Mise à jour de la hitbox du powerup pour suivre son déplacement
+        self.rect.x = self.x
+    
+    def check_collision(self, bird_rect):
+        # On regarde si la hitbox de l'oiseau est entrée en collision avec celle du powerup ou non
+        return self.rect.colliderect(bird_rect) 
+
+    def display_powerup(self, screen):
+        # On affiche les powerup
+        screen.blit(self.image, (self.x, self.y))
+    
+
+    @abstractmethod
+    def apply_effect(self):
+        pass
+        
+class anti_gravity(PowerUp):
+    def __init__(self, screen_width, screen_height, height, image):
+        super.__init__(screen_width, screen_height, height, image)
+    
+    def apply_effect(self):
+        # Inverser la gravité de l'oiseau
+        inverse_gravity()
+
